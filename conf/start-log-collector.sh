ca95cabda7f74717f519b0d6e8f362c287e8d9f7
ivan <ivanminutillo@outlook.it>
2025-09-30 19:52:27 +0200

    bare

--- conf/start-log-collector.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start background log collector for Docker containers
-cd __INSTALL_DIR__
-/usr/bin/docker-compose -f __INSTALL_DIR__/docker-compose.yml logs -f >> /var/log/__APP__/__APP__.log 2>&1 &
