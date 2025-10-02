b9665b47718ac03e0aaeb14ad6ac2c398865736e
ivan <ivanminutillo@outlook.it>
2025-10-02 10:33:50 +0200

    up

--- conf/bonfire-start.sh
+++ conf/bonfire-start.sh
@@ -13,4 +13,5 @@ set +a
 mkdir -p /var/log/__APP__
 
 # Start Bonfire and redirect all output to log file
-exec ./bin/bonfire start 2>&1 | tee -a /var/log/__APP__/startup.log
+# Note: Don't use 'exec' with pipes - it causes issues with process replacement
+./bin/bonfire start 2>&1 | tee -a /var/log/__APP__/startup.log
